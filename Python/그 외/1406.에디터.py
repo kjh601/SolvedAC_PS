f4c5c7b7e5389f9b77ad67889ca66994891bfd61
KJH <67515586+potatoholic@users.noreply.github.com>
2023-03-29 23:57:35 +0900

    edit 1406.에디터.py

--- Python/그 외/1406.에디터.py
+++ Python/그 외/1406.에디터.py
@@ -8,11 +8,17 @@ right = deque()
 N = int(input())
 for _ in range(N):
     cmd = input().split()
-    if cmd[0] == 'L' and left:
+    if cmd[0] == 'L':
+        if not left:
+            continue
         right.append(left.pop())
-    elif cmd[0] == 'D' and right:
+    elif cmd[0] == 'D':
+        if not right:
+            continue
         left.append(right.pop())
-    elif cmd[0] == 'B' and left:
+    elif cmd[0] == 'B':
+        if not left:
+            continue
         left.pop()
     elif cmd[0] == 'P':
         left.append(cmd[1])
