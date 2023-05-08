31ce01b3bb10f9a526bd2fdcd7db8ee26ecb87e9
KJH <67515586+potatoholic@users.noreply.github.com>
2023-05-08 22:14:32 +0900

    Feat: Add solution for 1000.A+B

---
+++ Java/Problem1000.java
@@ -0,0 +1,16 @@
+package Java;
+
+import java.util.Scanner;
+
+public class Problem1000 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+
+        System.out.println(A + B);
+
+        sc.close();
+    }
+}
