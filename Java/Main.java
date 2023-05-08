510cd97c05bbcb20770ed47e93b500622bf69aa2
KJH <67515586+potatoholic@users.noreply.github.com>
2023-05-08 11:33:25 +0900

    Feat: Add Main.java for Baekjoon submission

---
+++ Java/Main.java
@@ -0,0 +1,17 @@
+package Java;
+
+/*
+백준 문제 제출 양식
+1. 클래스명은 Main
+2. 패키지 없어야 함
+3. Main 이외의 클래스는 inner 클래스로
+4. Main 클래스에서 작성 시 모든 걸 static으로
+5. 빠르게 입력받기 : BufferedReader, StringTokenizer
+6. 입력을 위한 클래스는 하나만
+7. 빠르게 출력하기 : BufferedWriter, bf.write;
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
