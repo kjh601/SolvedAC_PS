752097bf3837ca184aeaa4a3d9833a20df4620c1
KJH <67515586+potatoholic@users.noreply.github.com>
2023-05-09 15:26:18 +0900

    Feat: Add solution for 1330.두 수 비교하기

---
+++ Java/Problem1330.java
@@ -0,0 +1,21 @@
+package Java;
+
+import java.util.Scanner;
+
+public class Problem1330 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+
+        if (A < B) {
+            System.out.println('<');
+        } else if (A == B) {
+            System.out.println("==");
+        } else {
+            System.out.println('>');
+        }
+        sc.close();
+    }
+}
