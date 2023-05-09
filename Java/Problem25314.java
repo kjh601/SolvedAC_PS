c17996412f1acab42b029f2acbc35092b1685ff9
KJH <67515586+potatoholic@users.noreply.github.com>
2023-05-09 19:10:39 +0900

    Feat: Add solution for 25314

---
+++ Java/Problem25314.java
@@ -0,0 +1,16 @@
+package Java;
+
+import java.util.Scanner;
+
+public class Problem25314 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        N/=4;
+        for (int i = 0; i < N; i++) {
+            System.out.print("long ");    
+        }
+        System.out.println("int");
+    }    
+}
