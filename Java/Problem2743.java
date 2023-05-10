ae602316da869c31b4312d57bff425a0f914e9c6
KJH <67515586+potatoholic@users.noreply.github.com>
2023-05-10 09:28:16 +0900

    Feat: Add 2743.단어 길이 재기

---
+++ Java/Problem2743.java
@@ -0,0 +1,13 @@
+package Java;
+
+import java.util.Scanner;
+
+public class Problem2743 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        System.out.println(str.length());
+        sc.close();
+    }
+    
+}
