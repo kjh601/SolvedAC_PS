7245e723f1183a9ef43cabd933fe525791ab9fc1
KJH <67515586+potatoholic@users.noreply.github.com>
2023-05-10 09:51:46 +0900

    Feat: Add solution for 11654.아스키 코드

---
+++ Java/Problem11654.java
@@ -0,0 +1,12 @@
+package Java;
+
+import java.util.Scanner;
+
+public class Problem11654 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String ASCII = sc.nextLine();
+        System.out.println(ASCII.charAt(0) + 0);
+        sc.close();
+    }
+}
