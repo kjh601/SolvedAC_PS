1c3cb7dc8b2bb1d020bc6924a5db8772fcca8cfc
KJH <67515586+potatoholic@users.noreply.github.com>
2023-05-21 22:53:14 +0900

    Feat: Add solution for 1086.부분합

--- Python/class 5/1086.부분합.py
+++ Python/class 5/1086.부분합.py
@@ -1,15 +1,18 @@
+import sys
+input = sys.stdin.readline
+
 n, s = map(int, input().split())
-arr = list(map(int, input().split()))
+arr = tuple(map(int, input().split()))
 
-start, end, subtotal, min_length = 0, 0, 0, 100001
+idx1, idx2, subtotal, min_length = 0, 0, 0, 100001
 
-while end < n:
-    while subtotal < s and end < n:
-        subtotal += arr[end]
-        end += 1
-    while subtotal >= s and start < n:
-        min_length = min(min_length, end - start)
-        subtotal -= arr[start]
-        start += 1
+while idx2 < n:
+    while subtotal < s and idx2 < n:
+        subtotal += arr[idx2]
+        idx2 += 1
+    while subtotal >= s and idx1 < n:
+        min_length = min(min_length, idx2 - idx1)
+        subtotal -= arr[idx1]
+        idx1 += 1
 
 print(0 if min_length == 100001 else min_length)
