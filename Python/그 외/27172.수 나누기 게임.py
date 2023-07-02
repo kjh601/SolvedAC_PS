6dd910560ed80368af180072e48716586db45407
KJH <fighter0628@gmail.com>
2023-07-02 22:35:03 +0900

    Feat: Add solution for 27172.수 나누기 게임

--- Python/그 외/27172.수 나누기 게임.py
+++ Python/그 외/27172.수 나누기 게임.py
@@ -1,1 +1,18 @@
-print(4*int(input()))
+N = int(input())
+
+arr = list(map(int, input().split()))
+
+scores = [0] * (1000001)
+card_exits = [False] * (1000001)
+
+for num in arr:
+    card_exits[num] = True
+
+for i in range(N):
+    for j in range(arr[i] * 2, 1000001, arr[i]):
+        if card_exits[j]:
+            scores[arr[i]] += 1
+            scores[j] -= 1
+
+for i in range(N):
+    print(scores[arr[i]], end=" ")
