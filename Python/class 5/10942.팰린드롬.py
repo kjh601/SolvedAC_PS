683d1aee5af328a9a3e042d3a708d1ea4cfb6920
KJH <fighter0628@gmail.com>
2023-07-08 23:41:13 +0900

    Feat: Add solution for 10942.팰린드롬?

--- Python/class 5/10942.팰린드롬.py
+++ Python/class 5/10942.팰린드롬.py
@@ -1,15 +1,25 @@
 import sys
 input = sys.stdin.readline
-print = sys.stdout.write
-
-dp = [0]*100001
 
 N = int(input())
 arr = list(map(int, input().split()))
-
-result = 0
 M = int(input())
-for _ in range(M):
-    S, E = map(int, input().split())
+dp = [[0]*N for _ in range(N)]
 
-    print(str(result))
+for i in range(N):
+    dp[i][i] = 1
+    if arr[i-1] == arr[i]:
+        dp[i-1][i] = 1
+
+for k in range(2, N):
+    for i in range(N-k):
+        j = i + k
+        if arr[i] == arr[j] and dp[i+1][j-1] == 1:
+            dp[i][j] = 1
+
+result = []
+for _ in range(M):
+    x, y = map(int, input().split())
+    result.append(dp[x-1][y-1])
+
+print('\n'.join(map(str,result)))
