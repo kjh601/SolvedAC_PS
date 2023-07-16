279491446cdf7e050b4f109f3e4ab5b16816c448
KJH <fighter0628@gmail.com>
2023-07-16 22:44:21 +0900

    Feat: Add solution for 알고리즘 수업

---
+++ Python/그 외/24313.알고리즘 수업 - 점근적 표기 1.py
@@ -0,0 +1,11 @@
+def is_big_o(a1, a0, c, n0):
+    for n in range(n0, 101):
+        if a1 * n + a0 > c * n:
+            return 0
+    return 1
+
+a1, a0 = map(int, input().split())
+c = int(input())
+n0 = int(input())
+
+print(is_big_o(a1, a0, c, n0))
