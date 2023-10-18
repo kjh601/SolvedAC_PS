fa109297a2bd0c5bc4a081821b767ba5b02d5d36
KJH <67515586+potatoholic@users.noreply.github.com>
2023-10-18 22:42:12 +0900

    Feat: Add solution for 2161.카드1

---
+++ Python/그 외/2161.카드1.py
@@ -0,0 +1,13 @@
+from collections import deque
+
+N = int(input())
+
+cards = deque(range(1, N + 1))
+
+discarded_cards = []
+
+while len(cards) > 1:
+    discarded_cards.append(cards.popleft())
+    cards.append(cards.popleft())
+
+print(' '.join(map(str, discarded_cards)), cards[0])
