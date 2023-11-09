195ef880c2bc33d26954c5e4b7a5490a5118edca
KJH <67515586+potatoholic@users.noreply.github.com>
2023-11-09 23:29:08 +0900

    Feat: Add solution for 6497.전력난

---
+++ Python/6497.전력난.py
@@ -0,0 +1,38 @@
+import sys
+input = sys.stdin.readline
+write = sys.stdout.write
+
+
+def findParent(parent, x):
+    if parent[x] == x:
+        return x
+    parent[x] = findParent(parent, parent[x])
+    return parent[x]
+
+
+def unionParent(parent, a, b):
+    a = findParent(parent, a)
+    b = findParent(parent, b)
+    if a < b:
+        parent[b] = a
+    else:
+        parent[a] = b
+
+
+while (True):
+    m, n = map(int, input().split())
+    if m == 0 and n == 0:
+        break
+    edge = sorted([tuple(map(int, input().split()))
+                   for _ in range(n)], key=lambda x: x[2])
+
+    parent = [i for i in range(m)]
+
+    total = sum(t[-1] for t in edge)
+    for v1, v2, w in edge:
+        if findParent(parent, v1) == findParent(parent, v2):
+            continue
+        unionParent(parent, v1, v2)
+        total -= w
+
+    write(str(total)+'\n')
